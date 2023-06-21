dec0636f728cd13a84aadb5fb35a47228510f1da
haroonsyed <haroonaminsyed@gmail.com>
2023-06-21 03:56:01 -0400

    Finished writing sanity tests for matrix lib

--- tests/matrix_tests.rs
+++ tests/matrix_tests.rs
@@ -22,6 +22,24 @@ mod matrix_tests {
   }
 
   #[test]
+  fn matrix_multiply() {
+    let test_data = Matrix {
+      data: vec![vec![2.0, 4.0], vec![1.0, 3.0]],
+    };
+    let test_data_2 = Matrix {
+      data: vec![vec![3.0, 1.0, 5.0], vec![-2.0, 1.0, 3.0]],
+    };
+
+    let expected_result = Matrix {
+      data: vec![vec![-2.0, 6.0, 22.0], vec![-3.0, 4.0, 14.0]],
+    };
+
+    let observed_result = test_data.matrix_multiply(&test_data_2);
+
+    assert!(matrix_are_equal(observed_result, expected_result));
+  }
+
+  #[test]
   fn transpose() {
     let test_data = Matrix {
       data: vec![vec![1.0, 2.0, 3.0], vec![4.0, 5.0, 6.0]],
@@ -31,6 +49,20 @@ mod matrix_tests {
     };
 
     let observed_result = test_data.transpose();
+
+    assert!(matrix_are_equal(observed_result, expected_result));
+  }
+
+  #[test]
+  fn element_apply() {
+    let test_data = Matrix {
+      data: vec![vec![1.0, 2.0, 3.0], vec![4.0, 5.0, 6.0]],
+    };
+    let expected_result = Matrix {
+      data: vec![vec![0.0, 1.0, 2.0], vec![3.0, 4.0, 5.0]],
+    };
+
+    let observed_result = test_data.element_apply(|x| x - 1.0);
 
     assert!(matrix_are_equal(observed_result, expected_result));
   }
